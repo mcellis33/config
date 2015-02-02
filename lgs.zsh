65fb14e07629e8bcdbdee39173c941e4f010b006
Mark Ellis <markellis@igneoussystems.com>
2015-02-02 11:06:52 -0800

    Add lgs and plgs aliases.

--- lgs.zsh
+++ lgs.zsh
@@ -1,1 +1,5 @@
-rm -rf lgs; mkdir lgs; for i in `docker ps | awk '{print $1}' | grep -v CONTAINER`; docker logs $i > lgs/$i;
+#!/bin/zsh
+rm -rf lgs;
+mkdir lgs;
+for i in `docker ps | awk '{print $1}' | grep -v CONTAINER`;
+	docker logs $i > lgs/$i;
